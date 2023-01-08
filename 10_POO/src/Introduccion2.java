a44dd6454a3e7b3edda5c455108918f103f0d7a9
isaac.calderon <isaaccalderon89@gmail.com>
2023-01-08 19:17:04 +0100

    5.2 argumento o parametro?

---
+++ 10_POO/src/Introduccion2.java
@@ -0,0 +1,47 @@
+
+public class Introduccion2 {
+
+	public static void main(String[] args) {
+		/*
+		 * El paradigma de programacion orientada a objetos define los programas en terminos de clases extraidos del 
+		 * lenguaje del problema original.
+		 * 
+		 * La programacion orientada a objetos cumple con estas caracteristicas: 
+		 * 
+		 * 1.ABSTRACION: posibilidad de aislar un elemento del resto de elementos que lo acompañanpara centrarnos
+		 * 				exclusivamente en él.
+		 * 2.ENCAPSULAMIENTO: ocultamiento de los datos de un objeto de manera que solo pueda acceder a ellos a traves de los 
+		 * 				métodos específicos para ese fin.
+		 * 3.JERARQUIA: un programa orientado a objetos se define en términos de "clases de objetos" que se relacionan
+		 * 				entre sí formando una jerarquía, que puede ser:
+		 * 		
+		 * 			* Por agregación: una clase contiene como propiedad un objeto de otra clase. Un objeto 'plaza contiene
+		 * 					un objeto 'coche'.
+		 * 			* Por herencia: una clase derivada (hija) heredatodas las propiedades y métodos de una clase base(madre).
+		 * 
+		 * 4.MODULARIDAD: capacidad de dividr una palicacion en partes más pequeñas denomidas módulos.
+		 * 5.POLIMORFISMO: siginifica multiples formas. El polimorfismo nos permite operar con objetos de diferente tipo como 
+		 * 				si fueran del mismo tipo, siempre que haya una relación entre ellos de tipo "es un/una".
+		 * 
+		 * Se va a explicar por medio de un ejemplo cómo funcionan las referencias a objetos en la clase coche.
+		 * 
+		 * REFERENCIA AL OBJETO ACTUAL: THIS
+		 * ---------------------------------
+		 * Dentro del código interno de una clase necesitamos hacer referencia al objeto actual. En java se consigue con THIS. 
+		 * Se puede ver un ejemplo en 'miCoche'.
+		 * 
+		 * INTERFAZ DE UNA FUNCIÓN
+		 * -----------------------
+		 * Las funciones on un conjunto de líneas de cófigo que realizan una determinada tarea cuando son invocadas o llamadas.
+		 * 
+		 * Cada función está dividida en dos partes:
+		 * 
+		 * 1. IMPLEMENTACIÓN DE LA FUNCIÓN: donde se desarrollan las instrucciones necesarias para llevar a cabo la tarea. 
+		 * 2. INTERFAZ DE LA FUNCIÓN: sistema de llamada a la función. Como usuarios, para realizar la llamada o inovación, solo nos interesa 
+		 * saber los parámetros de entrada requeridos y el valor que obtendremos, no los detalles de implementación.
+		 */
+
+	}
+
+}
+
--- 99-CuentaBanco/src/ejemplocoche/Principal.java
+++ 99-CuentaBanco/src/ejemplocoche/Principal.java
@@ -5,12 +5,23 @@ public class Principal {
 	public static void main(String[] args) {
 		
 		Coche miCoche = new Coche("5544PJK", "Ford", "Fiesta", "rojo", 0);
+		Coche miCoche2 = miCoche;
 		
 		miCoche.acelerar();
 		miCoche.acelerar();
+		miCoche2.acelerar();
+		miCoche2.acelerar();
 		
+		System.out.println("Mostrando información de miCoche");
 		System.out.println(miCoche.estadoCoche());
+		System.out.println("Mostrando información de miCoche2");
+		System.out.println(miCoche2.estadoCoche());
 		
+		/*
+		 * Hemos creado un objeto de la clase coche llamado miCoche. A continuación hemos creado 'Coche miCoche2 = miCoche;'
+		 * con esto no hemos creado un objeto nuevo, sino que estamos apuntando a la misma dirección de memoria.Por este 
+		 * motivo se accede al mismo objeto a través de las dos variables.
+		 */
 
 	}
 
